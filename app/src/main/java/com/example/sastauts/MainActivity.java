9a70c4ba9372fd478fcc4e2bd08ba97b8b7fd35e
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2024-03-10 23:47:44 +0530

    Toast + Fixes

--- app/src/main/java/com/example/sastauts/MainActivity.java
+++ app/src/main/java/com/example/sastauts/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.sastauts;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -9,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -26,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tabs);
         ViewPager2 viewPager = findViewById(R.id.view_pager);
 
-        Button helpButton = findViewById(R.id.help);
+        FloatingActionButton helpButton = findViewById(R.id.help);
 
         // frag pages adapter
         PageAdapter adapter = new PageAdapter(MainActivity.this);
